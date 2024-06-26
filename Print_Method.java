b6dd5948991d4cc276f2548ffefff28702c85246
Mahendra0115 <mahendrasing0115@gmail.com>
2024-06-26 08:19:56 +0530

    Java_Print_Methos

---
+++ Print_Method.java
@@ -0,0 +1,32 @@
+class Print_Method{
+ public static void main(String args[]){
+	 
+  // (print)--> we can pass varriable 
+  
+  
+  /* 
+      int a = 2;
+      int b = 3;
+  
+    System.out.print(a+b);
+	System.out.print(a-b);
+	System.out.print(a*b);
+	System.out.print(a/b);
+	
+  */
+  
+  // (printf)--> we can pass Extension Ex. (4+6) 
+  
+      System.out.println(5+2);
+	  System.out.println(5-2);
+	  System.out.println(5/2);
+	  System.out.println(5%2);
+	  
+	  
+
+  
+  
+  
+	
+ }
+}
