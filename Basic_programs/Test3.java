3dcc7e1940f1727740c40d49e7aeae69cfbb80f3
Mahendra0115 <mahendrasing0115@gmail.com>
2024-07-05 09:43:40 +0530

    operators , if_else, loops

---
+++ Basic_programs/Test3.java
@@ -0,0 +1,21 @@
+import java.util.*;   
+class Test4 {
+  
+public static void main(String args[])
+{
+     Scanner Sc = new Scanner(System.in);
+
+   System.out.print("Enter the number: ");
+   int num = sc.nextInt();
+
+
+     if( num > 0)
+   {
+      System.out.print("positive number: ");
+   } else{
+      System.out.print("Negative number: ");
+   } 
+
+
+   }
+}
