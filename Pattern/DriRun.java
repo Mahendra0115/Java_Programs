7dff0f3445edfd3398efaa8b6b00bb67805e23ea
Mahendra0115 <mahendrasing0115@gmail.com>
2024-07-10 16:41:12 +0530

    Updated

---
+++ Pattern/DriRun.java
@@ -0,0 +1,20 @@
+class DriRun{
+    
+    public static void main(String[] args) {
+        int i =0;
+        int j = 0;
+        // int num = 4;
+
+        for( i = 0; i< 2; i++){
+            for( j = 0; j< 3; j++){
+                // System.out.print( " A ");
+                for(int k = 0; k<1; k++){
+                    System.out.print( " A ");
+                }
+                
+            }
+           
+        }
+        System.out.println();
+    }
+}
--- Pattern/Squre.java
+++ Pattern/Squre.java
@@ -1,18 +1,23 @@
 class Squre {
     public static void main(String[] args) {
-        int no = 4;
+        int no = 3;
+        // cha no1 = 'B';
         int sum =0; 
 
         // Using nested loops
         for(int i =1; i<= no; i++){
+            
           for(int j=1; j<= no; j++){
-            System.out.print(" "+i);
+         
+            System.out.print(" A ");
+            System.out.print(" B ");
 
-            //  sum of frist  row 
+        //    System.out.print(" "+i);
                 if(i == 1){
                     sum = sum+i;
                 }
             }
+           
         System.out.println();
     }
 
